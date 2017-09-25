cdd850c1b2b4e3966155f2b35fb2d75e03840446
Stanis47 <petro.nawrik@gmail.com>
2017-09-25 08:50:28 +0300

    sprites

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Enemy : Creature {
+
+	Rigidbody2D rigEnemy;
+	public static Enemy enemy;
+	Player player;
+	bool attack;
+
+	void Start () {
+		enemy = GetComponent<Enemy>();
+		attack = false;
+		player = Player.player;
+		rigEnemy = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update () {
+		if(!attack)
+			//Move ();
+		Attack ();
+		Death ();
+	}
+
+	void Move(){
+		rigEnemy.velocity = new Vector2 (-1, rigEnemy.velocity.y);
+	}
+
+	protected override void Attack(){
+		if (player) {
+			if (Vector2.Distance (transform.position, player.transform.position) < 1f) {
+				attack = true;
+				player.GetHit (offence);
+			}
+		}
+	}
+
+	protected override void Death(){
+		/*if (health <= 0) {
+			Destroy (this);
+		}*/
+		/*if (Input.GetKeyUp (KeyCode.A)) {
+			anim.Play(enemy_death_1.name);
+			Debug.Log ("a");
+		}*/
+	}
+}
